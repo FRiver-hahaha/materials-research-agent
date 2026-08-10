aafbcb934a7c218e7fdeb920ebf1c463b0292e5c
FRiver <1203987745@qq.com>
2026-08-10 17:32:45 +0800

    add src/model/search_opt/__init__.py

---
+++ src/model/search_opt/__init__.py
@@ -0,0 +1,6 @@
+from .bayesian_opt import BayesianOptimizer
+from .genetic_search import GeneticSearch
+from .mcts import MCTS
+from .symbolic_regression import SymbolicRegression
+
+__all__ = ["BayesianOptimizer", "GeneticSearch", "MCTS", "SymbolicRegression"]
